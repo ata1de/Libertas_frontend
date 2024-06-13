68ba9beb6dfd43d3a5586910640c1b5c8b29bd6f
Mateus Ataide <teteulima@outlook.com>
2024-06-13 09:57:07 -0300

    Test3

---
+++ src/components/SheetDemo.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from './ui/sheet'
+import { Separator } from './ui/separator'
+import { Label } from '@radix-ui/react-label'
+import { MenuIcon } from 'lucide-react'
+import NavbarSheet from './navbarSheet'
+
+interface SheetDemoProps {
+    handleSearch: (event: React.FormEvent<HTMLFormElement>) => void;
+    setSearchName: (value: string) => void;
+    error: string;
+  
+}
+
+const SheetDemo = ({handleSearch, setSearchName, error}: SheetDemoProps) => {
+  return (
+    <Sheet>
+        <SheetTrigger asChild>
+          <MenuIcon className='block lg:hidden cursor-pointer' size={28} />
+        </SheetTrigger>
+        <SheetContent style={{backgroundColor: '#0F172A'}}>
+          <SheetHeader>
+            <SheetTitle className='flex gap-2'>
+              <img className='w-5' src="/logo_libertas.svg" alt="Foto das mãos de um advogado" />
+              <p className='uppercase text-2xl text-DarkRed font-bold font-sans'>LIBERTAS</p>
+            </SheetTitle>
+            <SheetDescription>
+              Browse our site through this area.
+            </SheetDescription>
+            <Separator className='w-auto'/>
+          </SheetHeader>
+          <NavbarSheet handleSearch={handleSearch} setSearchName={setSearchName} error={error}/>
+        </SheetContent>
+    </Sheet>
+  )
+}
+
+export default SheetDemo
